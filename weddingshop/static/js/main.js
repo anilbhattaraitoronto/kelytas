197e56505493730d3dd34598ef86cc3151247df5
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2020-02-20 11:47:41 -0500

    fixed javascript error and fixed styled zindex

--- weddingshop/static/js/main.js
+++ weddingshop/static/js/main.js
@@ -1,10 +1,11 @@
 let collectionBox = document.getElementById('collections')
 
-
-collections.addEventListener('click', function (e) {
-    if (e.target.nodeName == 'IMG') {
-        let imgBox = e.target.parentElement.parentElement.nextElementSibling.children[0]
-        let imgSrc = e.target.src
-        imgBox.src = imgSrc
-    }
-})
+if (collectionBox !== null) {
+    collections.addEventListener('click', function (e) {
+        if (e.target.nodeName == 'IMG') {
+            let imgBox = e.target.parentElement.parentElement.nextElementSibling.children[0]
+            let imgSrc = e.target.src
+            imgBox.src = imgSrc
+        }
+    })
+}
