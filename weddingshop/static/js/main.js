d1ff6334bd886e1b9533bafebf093290a2905611
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2020-02-27 12:16:24 -0500

    corrected the element to which the event is attached

--- weddingshop/static/js/main.js
+++ weddingshop/static/js/main.js
@@ -1,7 +1,7 @@
 let collectionBox = document.getElementById('collections')
 
 if (collectionBox !== null) {
-    collections.addEventListener('click', function (e) {
+    collectionBox.addEventListener('click', function (e) {
         if (e.target.nodeName == 'IMG') {
             let imgBox = e.target.parentElement.parentElement.nextElementSibling.children[0]
             let imgSrc = e.target.src
